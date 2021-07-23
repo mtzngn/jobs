5c05a034c70f7509938d37b7945a1ec60e3cdf21
taha <mt.zngn@gmail.com>
2021-07-23 12:40:14 +0100

    map and searching for job at that location via mock data implemented

---
+++ actions/job_actions.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import reverseGeocode from "latlng-to-zip";
+import qs from "qs";
+import JOB_DATA from "./IndeedJobData.json";
+
+import { FETCH_JOBS } from "./types";
+
+const JOB_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
+const JOB_QUERY_PARAMS = {
+  publisher: "4201738803816157",
+  form: "json",
+  v: "2",
+  latlong: 1,
+  radius: 10,
+  q: "javascript",
+};
+
+const buildJobsUrl = (zip) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
+export const fetchJobs = (region) => async (dispatch) => {
+  try {
+    // const zip = await reverseGeocode(region);
+    // const url = buildJobsUrl(zip);
+    // const { data } = await axios.get(url);
+    const data = JOB_DATA;
+
+    dispatch({ type: FETCH_JOBS, payload: data });
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,13 +1,64 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, ActivityIndicator, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
+import { connect } from "react-redux";
+import { Button } from "react-native-elements";
 
-const MapScreen = () => {
+import * as actions from "../actions";
+
+const MapScreen = (props) => {
+  const [region, setRegion] = useState({
+    longitude: -122,
+    latitude: 37,
+    longitudeDelta: 0.04,
+    latitudeDelta: 0.09,
+  });
+  const [mapLoaded, setMapLoaded] = useState(false);
+
+  useEffect(() => {
+    setMapLoaded(true);
+  }, []);
+
+  const onRegionChangeComplete = (region) => {
+    setRegion(region);
+  };
+  const onButtonPress = () => {
+    props.fetchJobs(region);
+  };
+
+  if (!mapLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
   return (
     <View style={{ flex: 1 }}>
-      <MapView style={{ flex: 1 }} />
+      <MapView
+        region={region}
+        style={{ flex: 1 }}
+        onRegionChangeComplete={onRegionChangeComplete}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Search This Area"
+          large
+          backgroundColor="#009688"
+          icon={{ name: "search" }}
+          onPress={onButtonPress}
+        />
+      </View>
     </View>
   );
 };
 
-export default MapScreen;
+const styles = StyleSheet.create({
+  buttonContainer: {
+    position: "absolute",
+    bottom: 20,
+    left: 0,
+    right: 0,
+  },
+});
+export default connect(null, actions)(MapScreen);
